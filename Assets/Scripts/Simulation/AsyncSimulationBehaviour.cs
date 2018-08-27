caccf708459a57ccf6b93c0f4934368328e29a72
szymon.brych@gmail.com <szymon.brych@gmail.com>
2018-08-27 20:40:31 +0100

    Removing interrupt.

--- Assets/Scripts/Simulation/AsyncSimulationBehaviour.cs
+++ Assets/Scripts/Simulation/AsyncSimulationBehaviour.cs
@@ -10,6 +10,8 @@ public class AsyncSimulationBehaviour : SimulationBehaviour<AsyncGameOfLife>
 
     private Thread[] threads;
 
+    private bool _cancelThreads = false;
+
     protected override void Start()
     {
         base.Start();
@@ -26,16 +28,16 @@ public class AsyncSimulationBehaviour : SimulationBehaviour<AsyncGameOfLife>
         {
             int fromY = i * height / numThreads;
             int toY = (i + 1) * height / numThreads;
-            threads[i] = new Thread(() => ThrededSimulate(new Vector2Int(0, fromY), new Vector2Int(width, toY)));
+            threads[i] = new Thread(() => ThrededSimulate(new Vector2Int(0, fromY), new Vector2Int(width, toY), canceller: this));
             threads[i].Start();
 
             Debug.Log(string.Format("Thread[i:{0},y0:{1},y1:{2}]",i,fromY,toY));
         }
     }
 
-    void ThrededSimulate(Vector2Int from, Vector2Int to)
+    void ThrededSimulate(Vector2Int from, Vector2Int to, AsyncSimulationBehaviour canceller)
     {
-        while (true)
+        while (!canceller._cancelThreads)
         {
             _gameOfLife.ProcessBoard(new IntPoint2D() { x = from.x, y = from.y }, new IntPoint2D() { x = to.x, y = to.y });
             Thread.Sleep(_multiThreadedUpdatetimeMs);
@@ -44,11 +46,13 @@ public class AsyncSimulationBehaviour : SimulationBehaviour<AsyncGameOfLife>
 
     private void OnDestroy()
     {
+        _cancelThreads = true;
+
         for (byte i = 0; i < numThreads; ++i)
         {
-            threads[i].Interrupt();
-
-            Debug.Log(string.Format("Thread[i:{0}] interrupted! Ouch!", i));
+            threads[i].Join();
         }
+
+        Debug.Log("All threads stopped");
     }
 }
